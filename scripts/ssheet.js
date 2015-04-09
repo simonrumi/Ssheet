06ffa7ebce791466a99b85888a62f8e2a0b35673
Simon Amarasingham <simon@dsonic.com>
2015-04-09 10:34:53 -0700

    UI is more consistent when toggling filtering
    when the filtering for rows or columns is on, the cell tools menu
    displays a differently worded menu item
    ...and the cell tools button is activated when either rows or columns
    are filtered

--- scripts/ssheet.js
+++ scripts/ssheet.js
@@ -78,7 +78,7 @@
 				} else {
 					throw 'Error: the cell id string ' + cellIdStr + ' was not in one of the expected formats';
 				}
-			},		
+			},
 			
 			/**
 			* @method getCurrentColumnForCellFromId When the user clicks on a "spacer" cell
@@ -110,7 +110,19 @@
 			getCurrentRowForCellFromId: function (cellIdStr, grid) {
 				var row = this.getRowFromCellId( cellIdStr );
 				return grid[row]['cells'];
-			}
+			},
+			
+			/**
+			* @method getCurrentCellFromId Return the single cell in the grid that is represented by the cellIdStr
+			* @param {String} cellIdStr The id of the cell in the format spacer_[row]_[column]
+			* @param {Object} grid The JSON object representing the grid
+			* @return {Object} the cell indicated byt the cellIdStr
+			*/
+			getCurrentCellFromId: function (cellIdStr, grid) {
+				var col = this.getColumnFromCellId( cellIdStr );
+				var row = this.getRowFromCellId( cellIdStr );
+				return grid[row].cells[col];
+			},
 			
 		}
 	}]);
@@ -265,22 +277,28 @@
 		
 		/**** tool menu hide/show stuff ********/
 		$scope.showToolMenu = false;
-		$scope.toggleToolMenuInCtrl = function () {
-			$scope.showToolMenu = !$scope.showToolMenu;
-			console.log('toggleToolMenuInCtrl: $scope.showToolMenu is now ' + $scope.showToolMenu);
-		}
+		// $scope.toggleToolMenuInCtrl = function () {
+		// 	$scope.showToolMenu = !$scope.showToolMenu;
+		// 	console.log('toggleToolMenuInCtrl: $scope.showToolMenu is now ' + $scope.showToolMenu);
+		// }
 		
 		$scope.showToolMenuInCtrl = function () {
 			$scope.showToolMenu = true;
 			console.log('showToolMenuInCtrl: $scope.showToolMenu is now ' + $scope.showToolMenu);
+			
+			//some cool animation of the dialog when it appears
+			TweenLite.from('#ss-cell-tool-menu', 1.5, {ease: Back.easeOut.config(1), scaleX:0, scaleY:0});
 		}
 		
-		//when the user clicks on the orange cell-tools button this will be updated with the id of its cell
-		$scope.idOfCurrentCell = '';
+		//when the user clicks on the orange cell-tools button this will be updated with the id of the cellTools button
+		$scope.idOfCurrentElement = '';
+		// ...and this will be given the value of the current data cell that the cell-tools button is associated with
+		$scope.currentCell = {};
 		
 		$scope.$on('UpdateIdOfCurrentCell', function (emittedObj) {
 			console.log('caught event UpdateIdOfCurrentCell...id is ' + emittedObj.targetScope.idOfCurrentElement);
-			$scope.idOfCurrentCell = emittedObj.targetScope.idOfCurrentElement;
+			$scope.idOfCurrentElement = emittedObj.targetScope.idOfCurrentElement;
+			$scope.currentCell = emittedObj.targetScope.cell;
 		});
 		
 	}]);
@@ -416,34 +434,58 @@
 	}]);
 
 	/**
-	* @class CellToolsController This is a Controller used by the Directive ssCellTools
+	* @class CellToolsController This is a Controller used by the Directive ssCellToolsBtn
 	*/
-	ssheet.controller('CellToolsController', ['filterByCellData', '$scope', function (filterByCellData, $scope) {
+	ssheet.controller('CellToolsController', ['cellFinder', '$scope', function (cellFinder, $scope) {
 		
 		$scope.onCellToolsClick = function ($event) {
 			console.log('onCellToolsClick: $event.pageX is ' + $event.pageX + ', $event.pageY is ' + $event.pageY);
 			
+			$scope.cell = cellFinder.getCurrentCellFromId( $scope.idOfCurrentElement, $scope.gridInCellToolsDirective );
+			
+			//send the id of the current cell to the GridController
+			$scope.$emit('UpdateIdOfCurrentCell'); 
+			
+			
+			///// QQQQ all this stuff below is to do with the display of the cellToolsDialog itself
+			/// would prefer to put it in there...somehow...
+			 
+			// position the cell menu next to the where the user clicked
 			$('#ss-cell-tool-menu').css('top', $event.pageY);
 			$('#ss-cell-tool-menu').css('left', $event.pageX + 10); ////BAAADD having magic nubmer in here. also bad having id ss-cell-tool-menu
-
+			
+			
+			// If the current cell is a row or column filter, display those menu items accordingly		
+			if ($scope.cell.isRowFilter) {
+				$('#row-filter').addClass('ss-cell-tools-menu-item-activated');
+			} else {
+				$('#row-filter').removeClass('ss-cell-tools-menu-item-activated');
+			}
+			
+			if ($scope.cell.isColumnFilter) {
+				$('#column-filter').addClass('ss-cell-tools-menu-item-activated');
+			} else {
+				$('#column-filter').removeClass('ss-cell-tools-menu-item-activated');
+			}
+			
+			////QQQ stuff above is to do with display of the CellToolsDialog...should go somewhere else
+			
+			
 			// make the tool menu visible
 			$scope.showToolMenuInDirective();
 			
-			//set the id of the current cell in the GridController
-			$scope.$emit('UpdateIdOfCurrentCell'); 
 		}
 	}]);
 
 	/**
-	* @class ssCellTools The Cell Tools menu comes up when clicking on the top, orange dot next to every cell in the grid 
+	* @class ssCellToolsBtn The Cell Tools menu comes up when clicking on the top, orange dot next to every cell in the grid 
 	*/ 
-	ssheet.directive('ssCellTools', [ function () {  
+	ssheet.directive('ssCellToolsBtn', [ function () {  
 			
 		return {
 			
 			restrict: 'E',
 			replace: true,
-			//transclude: true,
 			templateUrl: 'cellTools.html',
 			
 			scope: {
@@ -473,25 +515,44 @@
 			replace: true, 
 			transclude: true, /// this means that the content in between the tags <context-menu-dialog></context-menu-dialog> will be inserted in the template where "ng-transclude" appears
 			
-			link: function (scope, element, attrs) {
+			link: function (scope, element, attrs, ssCellToolsBtnCtrl) {
 				
 				scope.hideToolMenu = function() {
 					scope.showInDirective = false;
 				};
 				
+				/**
+				* @method updateCellToolsBtnActivatedState If any of the filters are turned on, then display the cellToolsBtn as activated
+				*/
+				scope.updateCellToolsBtnActivatedState = function () {
+					if (scope.$parent.currentCell.isColumnFilter || scope.$parent.currentCell.isRowFilter) {
+						$('#' + scope.$parent.idOfCurrentElement).addClass('ss-cell-tools-btn-activated');
+					} else {
+						$('#' + scope.$parent.idOfCurrentElement).removeClass('ss-cell-tools-btn-activated');
+					}
+				}
+				
 				scope.onColumnFilterClick = function () {
-					console.log('onColumnFilterClick: scope.$parent.idOfCurrentCell=' + scope.$parent.idOfCurrentCell);
-					$('#' + scope.$parent.idOfCurrentCell).toggleClass('ss-cell-tools-activated');
-					scope.$parent.grid = filterByCellData.toggleCellColumnFilter( scope.$parent.idOfCurrentCell, scope.$parent.grid );
+					console.log('onColumnFilterClick: scope.$parent.idOfCurrentElement=' + scope.$parent.idOfCurrentElement);
+					
+					// filter out columns in the grid according to the contents of this cell
+					scope.$parent.grid = filterByCellData.toggleCellColumnFilter( scope.$parent.idOfCurrentElement, scope.$parent.grid );
+					
+					// display the cellToolsBtn for this cell as activated
+					scope.updateCellToolsBtnActivatedState();
 					
 					// hide the cell tools menu
 					scope.showInDirective = false;
 				};
 				
 				scope.onRowFilterClick = function () {
-					console.log('onRowFilterClick: scope.$parent.idOfCurrentCell=' + scope.$parent.idOfCurrentCell);
-					$('#' + scope.$parent.idOfCurrentCell).toggleClass('ss-cell-tools-activated');
-					scope.$parent.grid = filterByCellData.toggleCellRowFilter( scope.$parent.idOfCurrentCell, scope.$parent.grid );
+					console.log('onRowFilterClick: scope.$parent.idOfCurrentElement=' + scope.$parent.idOfCurrentElement);
+					
+					// filter out rows in the grid according to the contents of this cell
+					scope.$parent.grid = filterByCellData.toggleCellRowFilter( scope.$parent.idOfCurrentElement, scope.$parent.grid );
+					
+					// display the cellToolsBtn for this cell as activated
+					scope.updateCellToolsBtnActivatedState();
 					
 					// hide the cell tools menu
 					scope.showInDirective = false;
